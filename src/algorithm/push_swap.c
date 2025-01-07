cf84606480253a2f92002f67f6f58e6c3bd3fd65
Mahmut Kilic <mahkilic@f1r1s6.codam.nl>
2025-01-07 16:23:08 +0100

    updates

--- src/algorithm/push_swap.c
+++ src/algorithm/push_swap.c
@@ -19,8 +19,6 @@ void	push_swap(t_stack **a, t_stack **b)
 	len_a = stack_len(*a);
 	if (len_a-- > 3 && !stack_sorted(*a))
 		pb(b, a, false);
-	if (len_a-- > 3 && !stack_sorted(*a))
-		pb(b, a, false);
 	while (len_a-- > 3 && !stack_sorted(*a))
 	{
 		init_nodes_a(*a, *b);
